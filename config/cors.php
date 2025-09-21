b8a28314167e6b3a645dcdbcef8d498ff9382be7
Ko-aungKo <yyaa82162@gmail.com>
2025-09-21 19:05:44 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -16,10 +16,14 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:5173', // Vite default port
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        // this is vercel link
+        'https://expense-tracker-kappa-ashen-90.vercel.app',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.+--expense-tracker-kappa-ashen-90\.vercel\.app$#',
+    ],
 
     'allowed_headers' => ['*'],
 
